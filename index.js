d635682bd27fb46eef3f1da7c8db9c285b3ec30e
Naomi Dennis <njdennis94@gmail.com>
2018-09-28 14:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Customer{
     return this.deliveries().map( (myDelivery) => { return myDelivery.meal() })
   }
   totalSpent(){
-    let x = this.meals().reduce( (accumulator, currentValue) => {  currentValue.price + accumulator; console.log(currentValue.price, accumulator) }, 0 )
+    let x = this.meals().reduce( (accumulator, currentValue) => {   console.log(currentValue.price, accumulator); return currentValue.price + accumulator; }, 0 )
 
     
   }
